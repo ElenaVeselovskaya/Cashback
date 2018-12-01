dea8b91dcfdb2f2122889e0993f5acd5a4b5f299
student <student@localhost>
2018-12-01 12:20:49 +0300

    Test limit added

--- app/cashback.py
+++ app/cashback.py
@@ -1,8 +1,12 @@
 def cashback(amount):
     percent = 0.05
     limit = 3_000
-    result =  amount * percent
+    result = amount * percent
     if result > limit:
         return limit
     return result
 
+
+def test_cashback_above_limit():
+    result = cashback(1_000_000)
+    assert 3_000 == result
